faa3ecf45a81c13ca4337ea71eec8cca18868393
brunocpbrito <brunocpbrito@gmail.com>
2021-01-26 15:17:37 -0300

    alterações para colocar fila de 40 vagas

--- perfil/PrimeiroPeriodo.cc
+++ perfil/PrimeiroPeriodo.cc
@@ -22,6 +22,8 @@ class PrimeiroPeriodo : public cSimpleModule {
     virtual void handleMessage(cMessage *msg) override;
     virtual void finish() override;
     virtual Aluno * calculaEvasao(Aluno *aluno);
+    virtual void filaDeAlunos(Aluno *aluno);
+    virtual Aluno * retorno();
 };
 
 Define_Module(PrimeiroPeriodo);
@@ -47,8 +49,13 @@ void PrimeiroPeriodo::handleMessage(cMessage *msg) {
 
     Aluno *aluno1 = dynamic_cast<Aluno *>(msg);
     Aluno *aluno2 = this->calculaEvasao(aluno1);
-    faltasStats.collect(1.0 * aluno2->getEvadido());
-    send(aluno2, "saida", portaSaida);
+    //faltasStats.collect(1.0 * aluno2->getEvadido());
+
+    //colocar numa fila de alunos (capacidade da turma de 40 alunos)
+    //enquanto a fila nao esta cheia, nao envia alunos para o prox periodo.
+    this->filaDeAlunos(aluno2);
+
+    //send(aluno2, "saida", portaSaida);
 }
 
 
@@ -90,5 +97,34 @@ void PrimeiroPeriodo::finish(){
     faltasStats.recordAs("Evasao");
 }
 
+//metodo para armazenar a capacidade de alunos numa turma.
+void PrimeiroPeriodo::filaDeAlunos(Aluno *aluno){
+
+    if (fila.isEmpty() || fila.getLength() < capacidadeFila) {
+
+         EV << "Aluno de numero \""<< aluno->getNome() << "\" colocado na turma .\n";
+         fila.insert(aluno);
+         EV << fila.getLength() << " alunos na turma.\n";
+
+         //supostamente demanda um tempo aqui para processar.
+         //trecho de codigo para fazer algo
+         simtime_t tempoServico = exponential(5.0);
+         EV << "Processando em " << tempoServico << "s." << endl;
+
+         //scheduleAt(simTime()+tempoServico, this->retorno());
+         sendDelayed(this->retorno(), simTime()+tempoServico,"saida", portaSaida);
+     } else {
+         EV << "capacidade da turma excedida .\n";
+         //colaca-se numa fila de espera.
+         //a fazer
+    }
+}
+
+Aluno * PrimeiroPeriodo::retorno(){
+    Aluno *retorno = new Aluno;
+    retorno = dynamic_cast<Aluno *>(fila.pop());
+    EV << "Enviando " << retorno->getNome() << " para o proximo periodo" << endl;
+    return retorno;
+}
 
 
